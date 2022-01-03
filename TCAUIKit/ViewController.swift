117baee0bffad9dc132aded224696bf7b51b4985
Tsungyu Yu <ytyubox@gmail.com>
2022-01-03 16:08:50 +0800

    Initial Commit

---
+++ TCAUIKit/ViewController.swift
@@ -0,0 +1,22 @@
+//
+/* 
+ *		Created by 游宗諭 in 2022/1/3
+ *		
+ *		Using Swift 5.0
+ *		
+ *		Running on macOS 12.1
+ */
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
